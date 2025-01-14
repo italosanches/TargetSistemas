8b914a38376a30755ea84b8002e5d1d3da805c3b
italosanches <italo_carvalho157@hotmail.com>
2025-01-14 20:27:05 -0300

    atividade 5

--- Atividade5/Program.cs
+++ Atividade5/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//5) Escreva um programa que inverta os caracteres de um string.
+
+//IMPORTANTE:
+//a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+//b) Evite usar funções prontas, como, por exemplo, reverse;
+//olait
+ReverterString("italo");
+ReverterString("Meu Texto");
+
+static void ReverterString(string texto)
+{
+    char[] caracteres = texto.ToCharArray();
+
+    for (int i = 0; i < caracteres.Length / 2; i++)
+    {
+
+        char temp = caracteres[i];
+        caracteres[i] = caracteres[caracteres.Length -i -1 ];
+        caracteres[caracteres.Length -1 -i ] = temp;
+    }
+
+    Console.WriteLine(new string(caracteres));
+}
